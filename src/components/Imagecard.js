03946a7537b1c6b0fa437076130634b45de0fd78
Subhratanu <subhratanusaha100@gmail.com>
2026-03-15 10:46:18 +0530

    feature: added accordion to the cards

--- src/components/Imagecard.js
+++ src/components/Imagecard.js
@@ -5,11 +5,10 @@
 function Imagecard({name, imgLink}) {
     console.log("This is the Imagecard component");
     return (
-        <div>
+        <>
             <h2>{name}</h2>
             <img src={imgLink} alt="placeholder" />
-        </div>
-        
+        </>
     );
 }
 
