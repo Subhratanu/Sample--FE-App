e3f48b598349e0d2feb2c119d5f8330bd36ece46
Subhratanu <subhratanusaha100@gmail.com>
2026-05-03 10:28:29 +0530

    feat: added routing files

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -8,6 +8,7 @@ const CardList = ({placeholder}) => {
     const [productData, setProductData] = useState([]);
     // Async Await way of fetching data
     useEffect(()=>{
+        //mounting phase
         fetch("https://api.escuelajs.co/api/v1/products")
         .then((res)=>res.json())
         .then((data)=>{
@@ -15,8 +16,15 @@ const CardList = ({placeholder}) => {
         }).catch((err)=>{
             console.log(err);
         });
+        return () =>{
+            // cleanup function, it will run when the component unmounts, it is used to clean up any side effects that may have been created in the useEffect hook, such as clearing timers, canceling network requests, or resetting state. It helps to prevent memory leaks and ensures that the component behaves correctly when it is removed from the DOM.
+            //unmounting phase
+            console.log("Unmounting....")
+            setProductData([]);
+        }
     },[]);// [] = dependency array, if empty then useEffect will run only once when the component mounts
 
+    // landing page > inner page > product page > product details page
 
     if(!productData?.length) return <Shimmer count={10} />;
     return (
